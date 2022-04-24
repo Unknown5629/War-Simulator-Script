740e6438caeea85e796e4830e1d827cba92dca26
Unknown5629 <76602042+Unknown5629@users.noreply.github.com>
2022-04-24 14:13:46 -0600

    Update Main War Simulator Script.lua

--- Main War Simulator Script.lua
+++ Main War Simulator Script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Unknown5629/War-Simulator-Script/main/War%20Simulator%20Main%20Script%20Part.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Unknown5629/War-Simulator-Script/main/War%20Simulator%20Main%20Script%20Part.lua", true))();
